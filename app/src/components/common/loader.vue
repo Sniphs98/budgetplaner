ff5431eaf57a3c9fde96280a74d258c7b0c4423f
Nils Haberkamp <n.haberkamp@shopware.com>
2021-03-06 13:40:18 +0100

    add login page

---
+++ app/src/components/common/loader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="loader-wrapper">
+    <div class="loader-backdrop"></div>
+    <div class="loader-spinner">
+      <b-spinner variant="primary"></b-spinner>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loader"
+}
+</script>
+
+<style scoped>
+.loader-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.loader-backdrop {
+  width: 100vw;
+  height: 100vh;
+  background: white;
+  opacity: 0.3;
+}
+
+.loader-spinner {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
